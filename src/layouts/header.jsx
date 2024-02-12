e46cc1dab1909a33b28ab9313963d1bc946a6f52
cd-badr <badr@gmail.com>
2024-02-12 22:45:13 +0100

    first sec + nav

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 export const Header = () => {
     return (
         <>
-        <nav className="flex flex-row items-center p-5 border-b-[1px] border-[#ffffff2a]">
-            <Link className='text-white no-underline' to={"/"}>
-                crypto
+        <nav className="flex flex-row items-center p-5 border-b-[1px] gap-10 border-[#ffffff2a] bg-[#ffffff00] z-10">
+            <Link className='text-white no-underline text-3xl' to={"/"}>
+                Logo
             </Link>
 
-            <div className="text-[#ffffff52] text-xl flex justify-center gap-3 w-full">
-                <Link>Home</Link>
-                <Link>About</Link>
-                <Link>Contact</Link>
+            <div className=" text-lg flex gap-5 w-full">
+                <Link className="text-[#ffffffb9]">Dashboard</Link>
+                <Link className=" text-[#ffffff76]">Markets</Link>
+                <Link className=" text-[#ffffff76]">Satake</Link>
+                <Link className=" text-[#ffffff76]">Governance</Link>
             </div>
         </nav>
             
